20fce59ceb9680459c1a98713e873115e8f1910d
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-07-04 13:53:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chapter_4_my_constraint_layout_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chapter_4_my_constraint_layout_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
